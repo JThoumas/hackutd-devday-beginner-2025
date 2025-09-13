d96d2b122cd502ca5568a13da877775b1cc4e369
John Pham <john@VP-MP.local>
2025-09-13 17:18:49 -0500

    Dev Day

--- src/app/components/courses.js
+++ src/app/components/courses.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import CourseCard from './coursecard'
+import data from '../data.json'
+
+// const data = [
+//   {
+//     "courseCode": "CS 1200",
+//     "title": "Introduction to Computer Science and Computer Software",
+//     "description": "Overiew of computer science",
+//     "creditHours": 2
+//   }
+// ];
 
 function Courses() {
   return (
     <div className="py-12 bg-gray-100">
+      {data.courses.map((course, idx) => (
+        <div key={idx} className="mb-6">
+          <CourseCard
+            courseCode={`${course.course_tag} ${course.course_number}`}
+            title={course.course_name}
+            desciption={course.course_description}
+            creditHours={course.course_description}
+          />
+
+        </div>
+      ))}
 
     </div>
   )
-}
+};
 
 export default Courses
